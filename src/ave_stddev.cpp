5771a34b318eeba63f79e5224e6df3c969a14c9c
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-03-07 22:26:28 +0100

    update solution to VS2017

--- src/ave_stddev.cpp
+++ src/ave_stddev.cpp
@@ -14,7 +14,6 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
 ************************************************************************/
 
-#define _SCL_SECURE_NO_WARNINGS
 #include <iostream>
 #include <fstream>
 #include <string>
